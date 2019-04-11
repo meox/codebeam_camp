7021c63804927a69b25642064b80a264af766fa9
Gian Lorenzo Meocci <glmeocci@gmail.com>
2019-04-11 20:27:40 +0200

    install distillery

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule CodebeamCamp.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:distillery, "~> 2.0", runtime: false},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
