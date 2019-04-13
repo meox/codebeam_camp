9e92d6d0023fcf2b2b2bf57aa56080342f33b2b7
Gian Lorenzo Meocci <glmeocci@gmail.com>
2019-04-13 13:31:04 +0200

    First page (prototyping)

--- lib/codebeam_camp_web/controllers/page_controller.ex
+++ lib/codebeam_camp_web/controllers/page_controller.ex
@@ -2,6 +2,29 @@ defmodule CodebeamCampWeb.PageController do
   use CodebeamCampWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", events: events())
+  end
+
+  def events do
+    [
+      %{
+        name: "Code BEAM STO",
+        date: "16-17 May 2019",
+        location: "Stockholm",
+        url: "https://codesync.global/conferences/code-beam-sto-2019/"
+      },
+      %{
+        name: "Code Elixir LDN",
+        date: "18 July 2019",
+        location: "London",
+        url: "https://codesync.global/conferences/code-elixir-ldn-2019/"
+      },
+      %{
+        name: "ElixirConf US",
+        date: "29-30 August 2019",
+        location: "Aurora, CO",
+        url: "https://elixirconf.com/2019/"
+      }
+    ]
   end
 end
